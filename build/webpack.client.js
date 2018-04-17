9832e3ca7df49585dae64e691bfbee9d72b89752
uhyo <uhyo@uhy.ooo>
2018-04-18 01:31:35 +0900

    Add Counter route

--- build/webpack.client.js
+++ build/webpack.client.js
@@ -2,10 +2,21 @@ const path = require('path');
 const base = require('./webpack.base.js');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 
+const history = require('connect-history-api-fallback');
+const convert = require('koa-connect');
+
 module.exports = Object.assign({}, base, {
   plugins: base.plugins.concat([
     new HTMLWebpackPlugin({
       template: path.join(__dirname, '../html/index.html'),
     }),
   ]),
+  serve: {
+    add: (app, middleware, options) => {
+      // webpack-serve setting for path fallbacks.
+      const historyOptions = {};
+
+      app.use(convert(history(historyOptions)));
+    },
+  },
 });
--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Router from 'vue-router';
 
 import Index from '../pages/index.vue';
+import Counter from '../pages/counter.vue';
 
 // Add the router middleware to Vue.
 Vue.use(Router);
@@ -13,6 +14,10 @@ export default new Router({
     {
       path: '/',
       component: Index,
+    },
+    {
+      path: '/:id([-0-9a-zA-Z_]{4,})',
+      component: Counter,
     },
   ],
 });
