77838a6ed94d1403b4dde365966062525f2a95c4
uhyo <uhyo@uhy.ooo>
2018-10-03 01:02:56 +0900

    styling for phones

--- src/components/two-column.tsx
+++ src/components/two-column.tsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import { MainContent } from './main-content';
+import { phone, notPhone } from './media';
 
 /**
  * Wrapper of two-column contents.
@@ -7,14 +8,22 @@ import { MainContent } from './main-content';
 export const TwoColumn = styled.div`
   display: flex;
   flex-flow: row nowrap;
+
+  ${phone`
+    flex-flow: row wrap;
+  `};
 `;
 
 export const SubContent = styled(MainContent)`
   flex: 50% 1 1;
-  margin: 2em 2em;
-  padding: 1em 1em 1px;
-  text-align: inherit;
-  h1 {
-    font-size: 1.5em;
-  }
+  ${phone`
+    flex: 100% 1 1;
+  `};
+  ${notPhone`
+    margin: 2em 1em;
+    padding: 1em 1em 1px;
+    h1 {
+      font-size: 1.5em;
+    }
+  `} text-align: inherit;
 `;
